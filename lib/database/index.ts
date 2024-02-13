1558ff259febd5e8cd0497b941983f0028122193
mldesignerjs <mldesignerjs@gmail.com>
2024-02-13 13:05:31 +0700

    fix-sycn-mongoo

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,7 +6,8 @@ let cached = (global as any).mongoose || { conn: null, promise: null }
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn
-    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing!')
+
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing')
 
     cached.promise =
         cached.promise ||
